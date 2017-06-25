caaca281ba62eabd3d20490a598f3a9a379d6191
DeepakMamdapure <deepak@bitcialtechnologies.com>
2017-06-25 17:13:35 +0530

    KOtlin First Hello World Program

---
+++ src/HelloWorld.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by Deepak mamdapure on 6/25/2017.
+ */
+
+
+/**
+ * Kotlin First Hello World print Program
+ */
+
+/**
+ * Main Function
+ * Kotlin Program Execution Start from Main , its as similar as like java 'main()' function.
+ */
+fun main(args: Array<String>) {
+    println("Hello World:")
+}
